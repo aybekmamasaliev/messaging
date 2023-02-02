6ee33393b1bb6b89813c3dd0602041b550d8febd
aybek <aybek.mamasaliev@bk.ru>
2023-02-02 06:54:38 +0600

    added mongoose

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-require("dotenv/config")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
 
-app.get("/", (req, res)=>{
-    res.send("hello")
-    console.log("it is working")
-})
+app.get("/", async (req, res) => {
+//   res.send("hello");
+    try {
+      const goods = await Goods.find();
+      res.json(goods);
+      console.log("it is working");
+    } catch (err) {
+      res.json({ message: err });
+    }
+  });
 
-const PORT=process.env.PORT || 3000
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  (err) => {
+    console.log("ok");
+  }
+);
 
-app.listen(PORT)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
